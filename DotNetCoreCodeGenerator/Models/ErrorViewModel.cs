040e2626cfb2483e53e79e07e6b1f628a46c2eba
EMIN YUCE <eminyuce@gmail.com>
2018-05-04 14:06:50 -0400

    First Commit

---
+++ DotNetCoreCodeGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCoreCodeGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
