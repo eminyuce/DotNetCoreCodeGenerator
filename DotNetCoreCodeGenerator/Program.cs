054087cc90ed03b015fcdfed72ef6c1c0ef77c75
EMIN YUCE <eminyuce@gmail.com>
2018-05-04 14:07:50 -0400

    Path changes

--- DotNetCoreCodeGenerator/Program.cs
+++ DotNetCoreCodeGenerator/Program.cs
@@ -12,15 +12,32 @@ namespace DotNetCoreCodeGenerator
 {
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    BuildWebHost(args).Run();
+
+
+        //}
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
+
+
         public static void Main(string[] args)
         {
+            //Console.WriteLine("Assembly.GetEntryAssembly().Location:" + Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location));
+            //Console.WriteLine("AppDomain.CurrentDomain.BaseDirectory:" + AppDomain.CurrentDomain.BaseDirectory);
+            //Console.WriteLine("System.IO.Directory.GetCurrentDirectory:" + System.IO.Directory.GetCurrentDirectory());
+
+
             BuildWebHost(args).Run();
-
-
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+             .UseContentRoot(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "/publish")
                 .UseStartup<Startup>()
                 .Build();
     }
